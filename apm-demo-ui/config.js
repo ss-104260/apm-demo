09f79b16dcfb1e7aadf9129ccc25458d01cf61d2
po-测试 <sq264692@alibaba-inc.com>
2020-07-27 16:21:35 +0800

    Update config.js

--- apm-demo-ui/config.js
+++ apm-demo-ui/config.js
@@ -1,6 +1,6 @@
 var endpointURL = process.env['ENDPOINT_URL'] || ""
 var scheme = process.env['DICE_PROTOCOL'] || "https"
-var apiURL = process.env['API_URL'] || "https://test-back.terminus-org.test.terminus.io";
+var apiURL = process.env['API_URL'] || "https://test-back.test.terminus.io";
 var testMode = process.env['TEST_MODE'] || "on"
 var tk = process.env['TERMINUS_KEY'] || "cb734d2bcbcc940178d5685d98f6c2c92";
 var collectURL = process.env['TERMINUS_TA_COLLECTOR_URL'] || "//analytics.terminus.io/collect";
